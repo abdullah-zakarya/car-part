8dbe0aebe3a82c662e0236525aba1b34dba26afc
abdullah zakaria <mohtarfana@gmail.com>
2024-10-06 07:46:59 +0300

    add user table schema

--- config/database.ts
+++ config/database.ts
@@ -1,13 +1,16 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DATABASENAME!,
-  process.env.BATABAES_USERNAME!,
+  process.env.DATABASE_NAME || "car_part",
+  process.env.DATABASE_USERNAME!,
   process.env.DATABASE_PASSWORD!,
   {
     host: "localhost",
     dialect: "postgres",
-    port: 4500,
+    port: Number(process.env.DATABASE_PORT)!,
   }
 );
 
--- server.ts
+++ server.ts
@@ -1,9 +1,29 @@
-import http from "http";
 import app from "./app";
 import dotenv from "dotenv";
-
+import sequelize from "./config/database";
+import User from "./modules/User";
 dotenv.config();
+sequelize
+  .sync()
+  .then((result) => {
+    oprate();
+    createUser().then((user) => console.log(user));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(process.env.PORT, () => {
-  console.log("server running on port " + process.env.PORT);
-});
+function oprate() {
+  app.listen(process.env.PORT, () => {
+    console.log("server running on port " + process.env.PORT);
+  });
+}
+
+async function createUser() {
+  const user = await User.create({
+    name: "samin",
+    email: "bomb@hmada.eiada",
+    password: "docktoerr",
+  });
+  return user;
+}
